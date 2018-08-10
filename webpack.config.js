c25bd1ec0bddfe03244b42712bccfe5deb284e09
James Cauwelier <james.cauwelier@gmail.com>
2018-08-10 13:47:13 +0200

    Fixes #1 by installing and configuration of react, babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "app.bundle.js"
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3456,
+    publicPath: "http://localhost:3456/dist/",
+    hotOnly: true
+  },
+  plugins: [ new webpack.HotModuleReplacementPlugin() ]
+};
